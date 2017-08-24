c5498a7d9a502e3c3c097785cae283ed861c5707
William Goulart <williamgoulart1@hotmail.com>
2017-08-24 02:06:02 -0300

    Create smoke tests & Code :metal:

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const search = () => {};
+const searchAlbums = () => {};
+const searchArtist = () => {};
+const searchTracks = () => {};
+const searchPlaylists = () => {};
+
+export { search, searchAlbums, searchArtist, searchTracks, searchPlaylists };
---
+++ tests/main.spec.js
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+import { search, searchAlbums, searchArtist, searchTracks, searchPlaylists } from '../src/main';
+
+describe('Spotfy Wrapper', () => {
+
+  describe('smoke tests', () => {
+
+    it('should exist the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('should exist the searchAlbums method', () => {
+      expect(searchAlbums).to.exist;
+    });
+
+    it('should exist the searchArtist method', () => {
+      expect(searchArtist).to.exist;
+    });
+
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('should exist the searchPlaylists', () => {
+      expect(searchPlaylists).to.exist;
+    });
+
+  });
+
+});
