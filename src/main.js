0a3fc07d44c48a051b1b13196f5508537c0e5317
William Goulart <williamgoulart1@hotmail.com>
2017-08-24 01:07:45 -0300

    Clean package.json, Init Tests & Codes

--- src/main.js
+++
@@ -1,5 +0,0 @@
-export const search = () => {};
-export const searchAlbums = () => {};
-export const searchArtitsts = () => {};
-export const searchPlaylists = () => {};
-export const searchTracks = () => {};
