28d22a327cb33cd9bd17a56f010df99016a6e36c
William Goulart <williamgoulart1@hotmail.com>
2017-08-24 00:59:15 -0300

    Add Search Functions

---
+++ src/main.js
@@ -0,0 +1,5 @@
+export const search = () => {};
+export const searchAlbums = () => {};
+export const searchArtitsts = () => {};
+export const searchPlaylists = () => {};
+export const searchTracks = () => {};
