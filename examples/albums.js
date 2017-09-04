e4e5fa61bf46125f43be00d0ebc198d64ec41143
William Goulart <williamgoulart1@hotmail.com>
2017-09-04 04:47:18 -0300

    Remove path examples

--- examples/albums.js
+++
@@ -1,7 +0,0 @@
-global.fetch = require('node-fetch');
-
-import { searchAlbums } from '../src/main';
-
-const albums = searchAlbums('acdc');
-
-albums.then(data => data.albums.items.map(item => console.log(item.name)));
